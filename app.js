874b6a9859dfc5ad4e8330963fe43d0cada88294
souvik zaminder <s.zaminder@gmail.com>
2016-12-04 13:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// Whole-script strict mode syntax
+"use strict";
 // Enemies our player must avoid
 var X = 0;
-var Enemy = function(x,y) {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
@@ -17,10 +19,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    var random = (Math.random() * 1000);
-    this.x = this.x + random * dt;
+    var random = (Math.random() * 200);
+    this.x += random * 5 * dt;
 
-    if (this.x >= 500) {
+    if (this.x >= 505) {
         this.x = X;
     }
 };
@@ -36,77 +38,77 @@ Enemy.prototype.render = function() {
 var Player = function() {
 
     this.sprite = 'images/char-boy.png';
-    this.x = 0;
-    this.y = 400;
+    this.x = 202;
+    this.y = 404;
 };
 
 Player.prototype.update = function() {
-        if (this.y < 1) {
+    if (this.y < -10) {
+      
+      this.reset(); 
+}
+this.checkCollisions();
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+ Player.prototype.checkCollisions = function() {
+
+        for (var i = 0, len = allEnemies.length; i < len ; i++) {
+
+        if (this.x + 50 >= allEnemies[i].x && this.x < allEnemies[i].x + 50 && this.y < allEnemies[i].y + 50  && this.y + 50>= allEnemies[i].y ) {
+            
             this.reset();
         }
+    }
+};
+
+Player.prototype.handleInput = function(move) {
+    if (move == 'left') {
+
+        this.x -= 101;
+    }
+
+    if (move == 'right' && this.x < 400) {
+
+        this.x += 101;
+    }
+    if (move == 'up') {
+
+        this.y -= 83;
+
+    }
+    if (move == 'down' && this.y < 400) {
+
+        this.y += 83;
+
+    }
+};
+
+Player.prototype.reset = function() {
+
+    this.x = 202;
+    this.y = 404;
+
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var allEnemies = [new Enemy(-150, 200), new Enemy(-160, 125), new Enemy(-150, 50)];
+
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
     };
-        Player.prototype.render = function() {
-            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        };
 
-        Player.prototype.collision = function() {
-
-            for (var i = 0; i < allEnemies.length; i++) {
-        
-            if (this.x + 50 > allEnemies[i].x && this.x < allEnemies[i].x + 50 && this.y < allEnemies[i].y + 50 && this.y + 50 > allEnemies[i].y ) {
-                
-                this.reset();
-            	}
-        	}
-        };
-
-        Player.prototype.handleInput = function(move) {
-            if (move == 'left') {
-
-                this.x -= 50;
-            }
-
-            if (move == 'right' && this.x < 400) {
-
-                this.x += 50;
-            }
-            if (move == 'up') {
-
-                this.y -= 50;
-
-            }
-            if (move == 'down' && this.y < 400) {
-                
-                this.y += 50;
-
-            }
-        };
-
-        Player.prototype.reset = function() {
-
-            this.x = 0;
-            this.y = 400;
-
-        };
-        // Now instantiate your objects.
-        // Place all enemy objects in an array called allEnemies
-        // Place the player object in a variable called player
-
-        var allEnemies = [new Enemy(-50,200), new Enemy(-50,130), new Enemy(-200,80)];
-
-        var player = new Player();
-
-        // This listens for key presses and sends the keys to your
-        // Player.handleInput() method. You don't need to modify this.
-        document.addEventListener('keyup', function(e) {
-            var allowedKeys = {
-                37: 'left',
-                38: 'up',
-                39: 'right',
-                40: 'down'
-            };
-
-            player.handleInput(allowedKeys[e.keyCode]);
-        });
-    
+    player.handleInput(allowedKeys[e.keyCode]);
+});
